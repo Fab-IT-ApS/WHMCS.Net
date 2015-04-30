b1ac3ad93c7c77f7c248a0f7933f9c40ef90bc52
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-04-30 13:20:24 +0200

    Serveral new Unit testing methods.
    Now using Moq and NUnit frameworks now implemented.

---
+++ WHMCS.EasyAPI/Whmcs.Tests/TestApiDataBroker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Specialized;
+using Moq;
+using NUnit.Framework;
+using Whmcs.Interfaces;
+
+namespace Whmcs.Tests
+{
+    [TestFixture]
+    public class TestApiDataBroker
+    {
+        [Test]
+        public void GetProductsByProductId_CallApiService()
+        {
+            // Arrange
+            const int testProductId = 1;
+            const string testOutputApiResponse = "testresponse";
+            var expectedJSON = new NameValueCollection
+            {
+                {"action", "getproducts"},
+                {"pid", testProductId.ToString()}
+            };
+
+            var mockApiService = new Mock<IApiService>();
+            mockApiService.Setup(a => a.GetData(expectedJSON)).Returns(testOutputApiResponse).Verifiable();
+
+            var mockJSONService = new Mock<IJSONService>();
+            var dataBroker = new ApiDataBroker(mockApiService.Object, mockJSONService.Object);
+
+            // Act
+            var actualResult = dataBroker.GetProductsByProductId(testProductId);
+
+            // Assert
+            mockApiService.Verify(a => a.GetData(expectedJSON));
+        }
+    }
+}
