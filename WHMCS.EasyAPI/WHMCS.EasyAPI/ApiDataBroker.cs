4903a4507c2923a0cb5c019386ae7ba3f1235e80
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-04-30 13:14:49 +0200

    Databroker now fully implemented and up to date.

--- WHMCS.EasyAPI/WHMCS.EasyAPI/ApiDataBroker.cs
+++ WHMCS.EasyAPI/WHMCS.EasyAPI/ApiDataBroker.cs
@@ -7,7 +7,7 @@ using Whmcs.Model.Products;
 
 namespace Whmcs
 {
-    class ApiDataBroker : IApiDataBroker
+    public class ApiDataBroker : IApiDataBroker
     {
         private readonly IApiService apiService;
         private readonly IJSONService jsonService;
@@ -30,33 +30,87 @@ namespace Whmcs
         }
         public ProductsResponse GetProductsByProductId(int productId)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getproducts"},
+                {"pid", productId.ToString()}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ProductsResponse>(apiResponse);
         }
         public ProductsResponse GetProductsByGroupId(int groupId)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getproducts"},
+                {"gid", groupId.ToString()}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ProductsResponse>(apiResponse);
         }
         public ProductsResponse GetProductsByModuleName(string moduleName)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getproducts"},
+                {"module", moduleName}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ProductsResponse>(apiResponse);
         }
 
         public ClientsResponse GetClients()
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getclients"},
+                {"limitstart","0"}, // Default Value in API = 0
+                {"limitnum","999"} // Default Value in API = 25
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ClientsResponse>(apiResponse);
         }
         public ClientsResponse SearchClientsByEmail(string searchArg)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getclients"},
+                {"limitstart", "0"}, // Default Value in API
+                {"limitnum", "25"}, // Default Value in API
+                {"search", searchArg}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ClientsResponse>(apiResponse);
         }
 
         public ClientDetailsResponse GetClientDetailsByClientId(int clientId, bool stats = true)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getclientsdetails"},
+                {"clientid", clientId.ToString()},
+                {"stats", stats.ToString()}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ClientDetailsResponse>(apiResponse);
         }
         public ClientDetailsResponse GetClientDetailsByEmail(string emailAddress, bool stats = true)
         {
-            throw new NotImplementedException();
+            var inputData = new NameValueCollection
+            {
+                {"action", "getclientsdetails"},
+                {"email", emailAddress},
+                {"stats", stats.ToString()}
+            };
+
+            var apiResponse = apiService.GetData(inputData);
+            return jsonService.DeserializeJSON<ClientDetailsResponse>(apiResponse);
         }
     }
 }
