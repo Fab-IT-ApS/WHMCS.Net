e7730ef41d258b9842de525fd9ce99ac15baa479
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-05-04 12:23:50 +0200

    Added Unit Test GetProductsByProductId();

--- WHMCS.EasyAPI/Whmcs.Tests/TestApiDataBroker.cs
+++ WHMCS.EasyAPI/Whmcs.Tests/TestApiDataBroker.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Specialized;
+using System.Linq.Expressions;
 using Moq;
 using NUnit.Framework;
 using Whmcs.Interfaces;
+using Whmcs.Model.Products;
 
 namespace Whmcs.Tests
 {
@@ -27,10 +29,35 @@ namespace Whmcs.Tests
             var dataBroker = new ApiDataBroker(mockApiService.Object, mockJSONService.Object);
 
             // Act
+            dataBroker.GetProductsByProductId(testProductId);
+            
+            // Assert
+            mockApiService.Verify(a => a.GetData(expectedJSON));
+        }
+
+        [Test]
+        public void GetProductsByProductId()
+        {
+            // Arrange
+            const int testProductId = 1;
+            const string testOutputApiResponse = "testresponse";
+            var testJSON = new NameValueCollection {{"test", "test"}};
+            var expectedResult = new ProductsResponse();
+
+            var mockApiService = new Mock<IApiService>();
+            mockApiService.Setup(a => a.GetData(testJSON)).Returns(testOutputApiResponse).Verifiable();
+
+            var mockJSONService = new Mock<IJSONService>();
+            mockJSONService.Setup(j => j.DeserializeJSON<ProductsResponse>(It.IsAny<string>())).Returns(expectedResult);
+
+            var dataBroker = new ApiDataBroker(mockApiService.Object, mockJSONService.Object);
+
+            // Act
             var actualResult = dataBroker.GetProductsByProductId(testProductId);
 
             // Assert
-            mockApiService.Verify(a => a.GetData(expectedJSON));
+            Assert.AreEqual(expectedResult, actualResult);
+            mockJSONService.Verify(j => j.DeserializeJSON<ProductsResponse>(It.IsAny<string>()));
         }
     }
 }
