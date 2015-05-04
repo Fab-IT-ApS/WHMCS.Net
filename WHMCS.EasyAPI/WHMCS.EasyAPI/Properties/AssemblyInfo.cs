09dbe24d650808146989e397e56dbc015e8a35c9
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-05-04 13:21:18 +0200

    Fixed Name & Added FAB:IT APS as the Asmbly Company

--- WHMCS.EasyAPI/WHMCS.EasyAPI/Properties/AssemblyInfo.cs
+++ WHMCS.EasyAPI/WHMCS.EasyAPI/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WHMCS.EasyAPI")]
+[assembly: AssemblyTitle("WHMCS.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WHMCS.EasyAPI")]
+[assembly: AssemblyCompany("Fab:IT ApS")]
+[assembly: AssemblyProduct("WHMCS.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
