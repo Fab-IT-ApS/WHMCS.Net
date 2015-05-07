23ce7747635dedc1691282b75fb8903dd6558c8b
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-05-07 14:07:01 +0200

    test creds are now read from .txt

--- WHMCS.EasyAPI/WHMCS.EasyAPI.Example/Program.cs
+++ WHMCS.EasyAPI/WHMCS.EasyAPI.Example/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq.Expressions;
 using Whmcs.Interfaces;
-using System.Configuration;
 
 namespace Whmcs.Example
 {
@@ -8,21 +9,31 @@ namespace Whmcs.Example
     {   
         static void Main()
         {
-            IApiService apiService = new ApiService(
-                ConfigurationManager.AppSettings["Username"],
-                ConfigurationManager.AppSettings["Password"],
-                ConfigurationManager.AppSettings["Domain"], 
-                Boolean.Parse(ConfigurationManager.AppSettings["Secure"]));
+            const string path = @"C:\Git\config files\whmcs.txt";
+            var username = GetLine(path, 1); // string
+            var password = GetLine(path, 2); // string
+            var domain = GetLine(path, 3); // string - ex: MyWhmcs.Installation.net
+            var secure = GetLine(path, 4); // bool - true = https, false = http
 
+
+            IApiService apiService = new ApiService(username, password, domain, bool.Parse(secure));
             IJSONService jsonService = new JSONService();
-
-
             IApiDataBroker apiDataBroker = new ApiDataBroker(apiService, jsonService);
 
             var whmcsApi = new WhmcsApi(apiDataBroker);
             var tmp = whmcsApi.GetProducts();
             Console.WriteLine(tmp);
 
+        }
+
+        private static string GetLine(string fileName, int line)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                for (int i = 1; i < line; i++)
+                    sr.ReadLine();
+                return sr.ReadLine();
+            }
         }
     }
 }
