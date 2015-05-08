39384cd688dc17f1a73e5c8ed259ac7564a8a1af
Jonathan Østrup-LT <jonathan@fab-it.dk>
2015-05-08 15:17:09 +0200

    fixed jsonproperty in contact

--- WHMCS.Net/WHMCS/Model/Contacts/Contacts.cs
+++ WHMCS.Net/WHMCS/Model/Contacts/Contacts.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace WHMCS.Model.Contacts
 {
     public class Contacts
     {
+        [JsonProperty("contact")]
         public List<Contact> contact { get; set; }
     }
 }
